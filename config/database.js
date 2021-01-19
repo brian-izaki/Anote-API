03b1b20ab9f7cc007e99d9914852ea8363970b0f
Brian_Izaki <bkizaki@outlook.com>
2021-01-19 18:03:50 -0300

    chore: database settings

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
 module.exports = {
   development: {
-    username: "root",
-    password: null,
+    username: "brian",
+    password: 1234,
     database: "database_development",
     host: "127.0.0.1",
     dialect: "postgres",
   },
   test: {
-    username: "root",
-    password: null,
+    username: "brian",
+    password: 1234,
     database: "database_test",
     host: "127.0.0.1",
     dialect: "postgres",
   },
   production: {
-    username: "root",
-    password: null,
+    username: "brian",
+    password: 1234,
     database: "database_production",
     host: "127.0.0.1",
     dialect: "postgres",
