359e1ce08b7a0261c43c7ecbc4e776483cce44eb
Brian_Izaki <bkizaki@outlook.com>
2021-01-23 19:19:54 -0300

    feat: add token route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express"),
   routeUser = require("./routes/user"),
+  routeToken = require("./routes/token"),
   app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(routeUser);
+app.use(routeToken);
 
 app.listen(3000, () => {
-  console.log("servidor iniciado")
+  console.log("servidor iniciado");
 });
