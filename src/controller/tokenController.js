d4f7c0a8bc1029694c297957928a56f372fac760
Brian_Izaki <bkizaki@outlook.com>
2021-02-04 18:27:23 -0300

    refactor: código

--- src/controller/tokenController.js
+++ src/controller/tokenController.js
@@ -1,26 +1,24 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const cfg = require("../../config/index");
+const { authenticate } = require("../factory/sequelize");
 
 module.exports = {
   async genToken(req, res) {
     const { email, password } = req.body;
 
     if (!(email && password)) {
-      res.status(412).json({ msg: "Não foi passado o email ou senha" });
-      return;
+      return res.status(412).json({ msg: "Não foi passado o email ou senha" });
     }
     try {
       const foundUser = await User.findOne({ where: { email } });
 
       if (!foundUser) {
-        res.status(404).json({ msg: "Email não foi encontrado" });
-        return;
+        return res.status(404).json({ msg: "Email não foi encontrado" });
       }
 
       if (!User.isPassword(password, foundUser.password)) {
-        res.status(412).json({ msg: "Senha inválida" });
-        return;
+        return res.status(412).json({ msg: "Senha inválida" });
       }
 
       const token = jwt.sign({ id: foundUser.id }, cfg.jwtSecret, cfg.opts);
